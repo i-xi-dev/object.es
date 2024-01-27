a40d16d85e8c7b5bf01d94e9ee4e85bc81a6401a
i-xi-dev <naoyuki.isono@gmail.com>
2024-01-27 15:40:28 +0900

    1.0.0

--- src/object.ts
+++ src/object.ts
@@ -1,3 +1,15 @@
 export function isNonNullObject(test: unknown): test is NonNullable<object> {
   return ((typeof test === "object") && (test !== null));
 }
+
+// deno-lint-ignore no-explicit-any
+export function isIterableObject<T = any>(test: unknown): test is Iterable<T> {
+  return isNonNullObject(test) && (Symbol.iterator in test);
+}
+
+// deno-lint-ignore no-explicit-any
+export function isAsyncIterableObject<T = any>(
+  test: unknown,
+): test is AsyncIterable<T> {
+  return isNonNullObject(test) && (Symbol.asyncIterator in test);
+}
--- tests/object.test.ts
+++ tests/object.test.ts
@@ -12,3 +12,46 @@ Deno.test("ObjectEx.isNonNullObject()", () => {
   assertStrictEquals(ObjectEx.isNonNullObject(true), false);
   assertStrictEquals(ObjectEx.isNonNullObject(false), false);
 });
+
+Deno.test("ObjectEx.isIterableObject()", () => {
+  assertStrictEquals(ObjectEx.isIterableObject({}), false);
+  assertStrictEquals(ObjectEx.isIterableObject(null), false);
+  assertStrictEquals(ObjectEx.isIterableObject(undefined), false);
+  assertStrictEquals(ObjectEx.isIterableObject([]), true);
+  assertStrictEquals(ObjectEx.isIterableObject(new Error()), false);
+  assertStrictEquals(ObjectEx.isIterableObject(""), false);
+  assertStrictEquals(ObjectEx.isIterableObject(1), false);
+  assertStrictEquals(ObjectEx.isIterableObject(true), false);
+  assertStrictEquals(ObjectEx.isIterableObject(false), false);
+
+  assertStrictEquals(ObjectEx.isIterableObject(new Uint8Array()), true);
+  assertStrictEquals(ObjectEx.isIterableObject(new String()), true);
+  assertStrictEquals(ObjectEx.isIterableObject((function* () {})()), true);
+  assertStrictEquals(
+    ObjectEx.isIterableObject((async function* () {})()),
+    false,
+  );
+});
+
+Deno.test("ObjectEx.isAsyncIterableObject()", () => {
+  assertStrictEquals(ObjectEx.isAsyncIterableObject({}), false);
+  assertStrictEquals(ObjectEx.isAsyncIterableObject(null), false);
+  assertStrictEquals(ObjectEx.isAsyncIterableObject(undefined), false);
+  assertStrictEquals(ObjectEx.isAsyncIterableObject([]), false);
+  assertStrictEquals(ObjectEx.isAsyncIterableObject(new Error()), false);
+  assertStrictEquals(ObjectEx.isAsyncIterableObject(""), false);
+  assertStrictEquals(ObjectEx.isAsyncIterableObject(1), false);
+  assertStrictEquals(ObjectEx.isAsyncIterableObject(true), false);
+  assertStrictEquals(ObjectEx.isAsyncIterableObject(false), false);
+
+  assertStrictEquals(ObjectEx.isAsyncIterableObject(new Uint8Array()), false);
+  assertStrictEquals(ObjectEx.isAsyncIterableObject(new String()), false);
+  assertStrictEquals(
+    ObjectEx.isAsyncIterableObject((function* () {})()),
+    false,
+  );
+  assertStrictEquals(
+    ObjectEx.isAsyncIterableObject((async function* () {})()),
+    true,
+  );
+});
